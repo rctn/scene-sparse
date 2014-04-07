8c3a33a87d67c4f6faddcf763ab6fe49292dc9b3
Shiry Ginosar <shiry@dhcp-46-240.eecs.berkeley.edu>
2014-04-07 16:56:12 -0700

    Adding code to analyze the correlation of each dataset

---
+++ data_analysis/get_data_correlation.m
@@ -0,0 +1,34 @@
+function C = get_data_correlation(img_dir, num_images, img_size, image_format, results_file)
+% usage:
+
+% create the data matrix where each image will be a column vector
+if (length(img_size) == 2)
+    M = img_size(1) * img_size(2);
+else
+    M = img_size(1) * img_size(2) * img_size(3);
+end
+X = zeros(M,num_images);
+idx = 1;
+
+% list all image directories
+image_directories = dir(img_dir);
+% for each image directory
+for i = 1:length(image_directories)
+    current_dir = image_directories(i).name;
+    if(isempty(strmatch('.',current_dir)))
+        current_path = [img_dir current_dir '/'];
+        % list all images in this directory
+        images = dir([current_path '/*' image_format]);
+        for j = 1:length(images)
+            current_image = images(j).name;
+            I = imread([current_path current_image], image_format);
+            % convert image to a column vector
+            X(:,idx) = reshape(I, [M 1]);
+            idx = idx + 1;
+        end
+    end
+end
+% compute the columnwise covariance between images in this dataset
+C = cov(X);
+save(results_file,C);
+end
