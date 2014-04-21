17176debd7f11286ed01fd3ced78ecb7443faaca
Mayur Mudigonda <mayur@ln002.calhpc.berkeley.edu>
2014-04-21 14:47:47 -0700

    Moved a bunch of stuff and edited the sh script

--- experiments/scene_sparse.sh
+++ experiments/scene_sparse.sh
@@ -7,7 +7,7 @@ source /usr/Modules/init/sh
 #SBATCH --mem-per-cpu 2GB
 #SBATCH --ntasks-per-node=4
 
-cd $HOME/scene-sparse
+cd $HOME/scene-sparse/experiments
 module load matlab
 
 #matlab -nodesktop -r "svm_test($lam,$overcomp,$cmpr,$start,$stop)"
