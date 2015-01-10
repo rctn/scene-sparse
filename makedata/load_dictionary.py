989827f6ceefd00a3121609d7943d0ce0cca693b
Shiry <shiry@eecs.berkeley.edu>
2015-01-09 20:33:47 -0800

    Fixed assertion

--- makedata/load_dictionary.py
+++ makedata/load_dictionary.py
@@ -7,5 +7,6 @@ def load_dictionary(filename_no_extension):
     :type filename_no_extension: A filename string without the .py extension
     """
     module = importlib.import_module(filename_no_extension)
-    assert isinstance(module.content, object)
+    assert isinstance(module, object)
+    assert isinstance(module.content, dict)
     return module.content
