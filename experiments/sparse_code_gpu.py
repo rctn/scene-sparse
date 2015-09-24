b59e89e99b4865c5aa61d1e1dab952d8a4776ac6
mudigonda <mayur.mudigonda@gmail.com>
2015-09-23 17:08:46 -0700

    This adds the FISTA inference algorithm from Alex's code into our repo. Now, trying to make it work. Probably will fail. Also theano. Unhappy debugging times

--- experiments/sparse_code_gpu.py
+++ experiments/sparse_code_gpu.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import os
 import utilities
+#FISTA updates
+##Courtesy Alexander G Anderson, 2015
 
 class LBFGS_SC:
 
@@ -48,6 +50,8 @@ class LBFGS_SC:
         #self.basis_prev_grad = theano.shared(self.basis_prev_grad)
         self.coeff_prev_grad = theano.shared(self.coeff_prev_grad)
         self.recon = theano.shared(0.0*self.data.get_value())
+        self.t_T = theano.shared(np.array([1.]).astype(theano.config.floatX), 'fista_T')
+        self.t_X = theano.shared(np.zeros(self.coeff.shape).astype(
         print('Compiling theano inference function')
         #self.infer_coeff_gd = self.create_infer_coeff_gd()
         self.f = self.create_coeff_fn()
@@ -76,6 +80,125 @@ class LBFGS_SC:
         active = len(res.x[np.abs(res.x)>1e-2])/float(self.basis_no*self.batch)
         print('Number of Active coefficients is ...',active)
         return active,res.fun 
+
+    def fista_init(self):
+        '''
+        This function we can just leave it calling Alex's original fista code. The rest
+        of it, we can modify to fit into our code base. This function is to be called once
+        at the start of the script. So maybe this goes into init?
+        Code Adapted From: Alexander G Anderson
+        '''
+        #This calls the fista_update function and creates a function handle
+        #fist_updates = fista_updates()
+        #_, t_fista_X, t_T = fist_updates.keys()
+
+        fista_step = theano.function(inputs = [],
+                                     #outputs = [t_E, t_E_rec, t_E_sp, t_SNR], #These need to be coded up
+                                     outputs = [],
+                                     updates = self.fista_updates)
+    def calculate_fista_L(self):
+        """
+        Calculates the 'L' constant for FISTA for the dictionary in t_D.get_value()
+        Code adapted from: Alexander G Anderson
+        """
+        D = self.basis.get_value()
+        try:
+            L = (2 * eigh(np.dot(D, D.T), eigvals_only=True, eigvals=(N_sp-1,N_sp-1))[0]).astype('float32')
+        except ValueError:
+            L = (2 * D_std ** 2 * N_sp).astype('float32') # Upper bound on largest eigenvalue
+        return L
+
+    def reset_fista_variables(self):
+        """
+        Resets fista variables
+        Code adapted from: Alexander G Anderson
+        """
+        A0 = np.zeros_like(self.coeff.get_value()).astype(theano.config.floatX)
+        self.coeff.set_value(A0)
+        self_t_X.set_value(A0)
+        self.t_T.set_value(np.array([1.]).astype(theano.config.floatX))
+        return 1
+
+    def fista_updates(self, pos_only=False):
+        """
+        Code to generate theano variables to minimize
+            t_E = t_E_rec + t_Alpha * T.sum(abs(t_A))
+        with respect to t_A using FISTA
+        t_A - Theano shared variable to minimize with respect to
+        t_E_rec - Theano Energy function to minimize, not including abs(t_A) term
+        t_L - Theano variable for the Lipschitz constant of d t_E / d t_A
+        pos_only - Boolean to say if we should do positive only minimization
+        Return - a dictionary of updates to run fista to pass to theano.function
+        Note: The auxillary variables needed by the algorithm must be
+            reset after each run
+        t_A = t_fista_X = A0, t_T = 1
+        See the end of the script for a sample usage
+        Code adapted from: Alexander G Anderson, 2015
+        """
+
+        def threshold(t_X):
+            """
+            Threshold function
+            """
+            if pos_only = True:
+                return T.switch(t_X > 0., t_X, 0.)
+            else:
+                return t_X
+
+        t_E_rec = (self.data - self.basis.dot(self.coeff))**2
+        t_E_sp = self.lam * T.abs_(coeff).sum(axis=0).mean()
+        t_E = t_E_rec + t_E_sp
+        #t_E_rec needs to be coded up here, since we don't actually have that variable anywhere
+        t_B = self.coeff - (1. / self.L) * T.grad(t_E_rec, self.coeff)
+        t_C = T.abs_(t_B) - self.lam / t_L
+        t_A_ista = T.switch(t_B > 0, 1., -1.) * threshold(t_C)
+
+        if pos_only:
+            t_A_ista = threshold(t_A_ista)
+        t_A_ista.name = 'A_ista'
+
+        t_T = theano.shared(np.array([1.]).astype(theano.config.floatX), 'fista_T')
+        shape = self.coeff.get_value().shape
+        t_X = theano.shared(np.zeros(shape).astype(
+            theano.config.floatX), 'fista_X')
+
+        t_X1 = t_A_ista
+        t_T1 = 0.5 * (1 + T.sqrt(1. + 4. * self.t_T ** 2))
+        t_T1.name = 'fista_T1'
+        t_A1 = t_X1 + (t_T1[0] - 1) / t_T[0] * (t_X1 - self.t_X)
+        t_A1.name = 'fista_A1'
+        updates = OrderedDict()
+        updates[self.coeff] = t_A1
+        updates[self.t_X] = t_X1
+        updates[self.t_T] = t_T1
+
+        return updates
+
+    def infer_fista(self,show_costs = False):
+        """
+        Code to train a sparse coding dictionary
+        N_itr - Number of iterations, a new batch of images for each
+        Alpha - Sparsity cost parameter... E = E_rec + Alpha + |A|_1
+        cost_list - list to which the cost at each iteration will be appended
+        N_g_itr - number of gradient steps in FISTA
+        show_costs - If true, print out costs every N_itr/10 iterations
+        Returns I_idx - indices corresponding to the most recent image batch,
+            to be used later in visualizations
+        """
+        if show_costs:
+            print 'Iteration, E, E_rec, E_sp, SNR'
+        self.reset_fista_variables()
+        self.calculate_fista_L()
+        for _ in range(N_g_itr):
+            E, E_rec, E_sp, SNR = self.fista_step( )
+        #dictionary_learning_step(Alpha, Eta, D_std, I_idx)
+        # 
+        #E, E_rec, E_sp, SNR = costs(I_idx, Alpha)
+        cost_list.append(E)
+        if ((i + 1) % (1 + N_itr / 10) == 0) and show_costs:
+            print i, E, E_rec, E_sp, SNR
+        return I_idx
+
 
     def create_infer_coeff_gd(self):
         r_err = (self.data - self.basis.dot(self.coeff))**2
